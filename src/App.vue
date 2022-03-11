8b93afeb6da807f3eefe88ec3a6de21b34a15004
VitaliyPliss <v.pliss@mail.ru>
2022-03-11 15:54:25 +0300

    тестовое МассПроект

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,115 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="promo">
+      <div class="promo__left">
+
+        <Burger />
+
+        <div class="promo__menu">
+          <ul>
+            <li class="promo__menu-item">Подсказки</li>
+            <li class="promo__menu-item">Подсказки</li>
+            <li class="promo__menu-item promo__menu-item--active">Подсказки</li>
+            <li class="promo__menu-item">Подсказки</li>
+          </ul>
+        </div>
+        <div class="promo__scroll">Скрольте вниз</div>
+      </div>
+
+      <Logo />
+
+      <div class="promo__sidelink">
+        <a href="#">hd.kinopoisk.ru</a>
+      </div>
+
+      <h1 class="promo__title">ОНИ ДОЛЖНЫ ЧТО-ТО ЗНАТЬ</h1>
+
+      <div class="promo__slider">
+        <div class="swiper">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <p class="swiper-text">
+                Для примера мы показали вам его лицо. В первой серии он прячется
+                в подвале<br />
+                за мониторами, и пусть борода не собьёт вас с толку. Найдите
+                героя и нажмите на паузу —<br />
+                ему не терпится отдать вам промокод.
+              </p>
+            </div>
+            <div class="swiper-slide">
+              <p class="swiper-text">
+                Для примера мы показали вам его лицо. В первой серии он прячется
+                в подвале<br />
+                за мониторами, и пусть борода не собьёт вас с толку. Найдите
+                героя и нажмите на паузу —<br />
+                ему не терпится отдать вам промокод.
+              </p>
+            </div>
+            <div class="swiper-slide">
+              <p class="swiper-text">
+                Для примера мы показали вам его лицо. В первой серии он прячется
+                в подвале<br />
+                за мониторами, и пусть борода не собьёт вас с толку. Найдите
+                героя и нажмите на паузу —<br />
+                ему не терпится отдать вам промокод.
+              </p>
+            </div>
+          </div>
+          <div class="swiper-pagination"></div>
+
+          <div class="swiper-button-prev"></div>
+          <div class="swiper-button-next"></div>
+        </div>
+      </div>
+
+      <a href="#" class="promo__btn">Искать промокод</a>
+
+      <div class="promo__backgrounds"></div>
+      
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Logo from "@/components/Logo.vue";
+import Burger from "@/components/Burger.vue";
+import Swiper, { Navigation, Pagination } from "swiper";
+import "swiper/swiper.scss";
+import "swiper/modules/navigation/navigation.scss";
+import "swiper/modules/pagination/pagination.scss";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    Logo,
+    Burger,
+  },
+  mounted: function () {
+    const mySwiper = new Swiper(".swiper", {
+      modules: [Navigation, Pagination],
+      autoplay: false,
+      loop: false,
+      draggable: true,
+
+      pagination: {
+        el: ".swiper-pagination",
+        type: "fraction",
+      },
+
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+    });
+    console.log(mySwiper + 'init');
+  },
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/assets/scss/app";
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="logo">
+    <a href="#">
+      <img src="@/assets/images/logo.svg" width="182" height="25" alt="kinopoisk">
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Logo"
+}
+</script>
+
+<style>
+
+</style>
